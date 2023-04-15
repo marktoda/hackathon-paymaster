4227baf91efb6084301354244bb5773fa71fd9fd
Eric Zhong <eric.zhong@uniswap.org>
2023-04-16 00:00:07 +0900

    Remove deposit stuff

--- src/GeneralPaymaster.sol
+++ src/GeneralPaymaster.sol
@@ -27,6 +27,7 @@ contract GeneralPaymaster is BasePaymaster, ERC1155Supply, AaveFundsManager {
     error InsufficientETH();
     error NotLocked();
     error InsufficientDeposit();
+    error PostOpError();
 
     // eth for gas by token
     mapping(address token => uint256) public tokenETHBalance;
@@ -36,17 +37,12 @@ contract GeneralPaymaster is BasePaymaster, ERC1155Supply, AaveFundsManager {
 
     IOracle private constant NULL_ORACLE = IOracle(address(0));
     mapping(IERC20 => IOracle) public oracles;
-    mapping(IERC20 => mapping(address => uint256)) public balances;
-    mapping(address => uint256) public unlockBlock;
 
     constructor(IEntryPoint _entryPoint)
         BasePaymaster(_entryPoint)
         AaveFundsManager(msg.sender, _entryPoint)
         ERC1155("")
-    {
-        // owner account is unblocked, to allow withdraw of paid tokens;
-        unlockTokenDeposit();
-    }
+    {}
 
     /// @notice Deposit ETH to participate as a Sub-Paymaster for the given token
     /// @param token The token to accept in return for ETH
@@ -80,56 +76,6 @@ contract GeneralPaymaster is BasePaymaster, ERC1155Supply, AaveFundsManager {
     function addToken(IERC20 token, IOracle tokenPriceOracle) external onlyOwner {
         if (oracles[token] != NULL_ORACLE) revert TokenAlreadySet();
         oracles[token] = tokenPriceOracle;
-    }
-
-    /// @notice deposit tokens that a specific account can use to pay for gas.
-    /// The sender must first approve this paymaster to withdraw these tokens (they are only withdrawn in this method).
-    /// Note depositing the tokens is equivalent to transferring them to the "account" - only the account can later
-    ///  use them - either as gas, or using withdrawTo()
-    /// @param token the token to deposit.
-    /// @param account the account to deposit for.
-    /// @param amount the amount of token to deposit.
-    function addDepositFor(IERC20 token, address account, uint256 amount) external {
-        //(sender must have approval for the paymaster)
-        token.safeTransferFrom(msg.sender, address(this), amount);
-        if (oracles[token] == NULL_ORACLE) revert UnsupportedToken();
-        balances[token][account] += amount;
-        if (msg.sender == account) {
-            lockTokenDeposit();
-        }
-    }
-
-    ///  @notice fetch deposit info for the given address
-    ///  @return amount - the amount of given token deposited to the Paymaster.
-    /// @return _unlockBlock - the block height at which the deposit can be withdrawn.
-    function depositInfo(IERC20 token, address account) public view returns (uint256 amount, uint256 _unlockBlock) {
-        amount = balances[token][account];
-        _unlockBlock = unlockBlock[account];
-    }
-
-    /// @notice unlock deposit, so that it can be withdrawn.
-    /// can't be called in the same block as withdrawTo()
-    function unlockTokenDeposit() public {
-        unlockBlock[msg.sender] = block.number;
-    }
-
-    /// @notice lock the tokens deposited for this account so they can be used to pay for gas.
-    /// after calling unlockTokenDeposit(), the account can't use this paymaster until the deposit is locked.
-    function lockTokenDeposit() public {
-        unlockBlock[msg.sender] = 0;
-    }
-
-    /// @notice withdraw tokens.
-    /// can only be called after unlock() is called in a previous block.
-    /// @param token the token deposit to withdraw
-    /// @param target address to send to
-    /// @param amount amount to withdraw
-    function withdrawTokensTo(IERC20 token, address target, uint256 amount) public {
-        if (unlockBlock[msg.sender] == 0 || block.number <= unlockBlock[msg.sender]) {
-            revert NotUnlocked();
-        }
-        balances[token][msg.sender] -= amount;
-        token.safeTransfer(target, amount);
     }
 
     /// @notice translate the given eth value to token amount
@@ -168,8 +114,6 @@ contract GeneralPaymaster is BasePaymaster, ERC1155Supply, AaveFundsManager {
         uint256 maxTokenCost = getTokenValueOfEth(token, maxCost);
         uint256 gasPriceUserOp = userOp.gasPrice();
         if (tokenETHBalance[address(token)] < maxCost) revert InsufficientETH();
-        if (unlockBlock[account] != 0) revert NotLocked();
-        if (balances[token][account] < maxTokenCost) revert InsufficientDeposit();
         return (abi.encode(account, token, gasPriceUserOp, maxTokenCost, maxCost), 0);
     }
 
@@ -190,11 +134,8 @@ contract GeneralPaymaster is BasePaymaster, ERC1155Supply, AaveFundsManager {
             // attempt to pay with tokens:
             token.safeTransferFrom(account, address(this), actualTokenCost);
         } else {
-            //in case above transferFrom failed, pay with deposit:
-            balances[token][account] -= actualTokenCost;
+            revert PostOpError();
         }
-        balances[token][address(this)] += actualTokenCost;
-
         _deposit(address(token), actualTokenCost);
     }
 }
